9815020864121077392711ac1ce74f8dd4104884
ismayilzadesenan <senanss1998@gmail.com>
2021-04-02 01:29:36 +0500

    dropdown bitti

---
+++ PROJECT/script/app.js
@@ -0,0 +1,7 @@
+let searchclick=document.querySelector('#tb');
+let  bodys=document.querySelector('body');
+searchclick.addEventListener('click',function(){
+ bodys.style.backgroundColor='red';
+    console.log("red");
+})
+
